484d0e4e9782125caed77db86180d203b0e8fa9f
joshau kregg <kreggjoshua@gmail.com>
2023-03-20 14:02:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    {year: 2015, result: "W"},
+    {year: 2014, result: "N/A"},
+    {year: 2013, result: "L"}
+]
+
+
+function superbowlWin(array) {
+    if(array.find(arr => arr.result === "W")){
+        return array.find(arr => arr.result === "W").year;
+    }
+    else{
+        return undefined
+    }
+    
+}
+
+console.log(array.find(arr => arr.result === "W"));
